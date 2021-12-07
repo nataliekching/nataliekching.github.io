635f8e5f02e38c54d0662ce56b6d6c52bdb3b024
nataliekching <71060017+nataliekching@users.noreply.github.com>
2021-12-06 23:51:01 -0500

    Add files via upload

---
+++ medium_massage.js
@@ -0,0 +1,219 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", ()=>{
+
+    if(btn.innerText === "The Meaning in the Massage"){
+        btn.innerText = "Societies have always been shaped more by the nature of the media by which men communicate than by the content of the communication.";
+    }else{
+        btn.innerText = "The alphabet, for instance, is a technology that is absorbed by the very young child in a completely unconcious manner, by osmosis so to speak";
+    }
+});
+const btnA = document.getElementById("btnA");
+
+btnA.addEventListener("click", ()=>{
+
+    if(btnA.innerText === "A"){
+        btnA.innerText = "Do";
+    }
+});
+const btnB = document.getElementById("btnB");
+
+btnB.addEventListener("click", ()=>{
+
+    if(btnB.innerText === "B"){
+        btnB.innerText = "You";
+    }
+});
+const btnC = document.getElementById("btnC");
+
+btnC.addEventListener("click", ()=>{
+
+    if(btnC.innerText === "C"){
+        btnC.innerText = "Actually";
+    }
+});
+const btnD = document.getElementById("btnD");
+
+btnD.addEventListener("click", ()=>{
+
+    if(btnD.innerText === "D"){
+        btnD.innerText = "Understand";
+    }
+});
+const btnE = document.getElementById("btnE");
+
+btnE.addEventListener("click", ()=>{
+
+    if(btnE.innerText === "E"){
+        btnE.innerText = "What";
+    }
+});
+const btnF = document.getElementById("btnF");
+
+btnF.addEventListener("click", ()=>{
+
+    if(btnF.innerText === "F"){
+        btnF.innerText = "You";
+    }
+});
+const btnG = document.getElementById("btnG");
+
+btnG.addEventListener("click", ()=>{
+
+    if(btnG.innerText === "G"){
+        btnG.innerText = "Are";
+    }
+});
+const btnH = document.getElementById("btnH");
+
+btnH.addEventListener("click", ()=>{
+
+    if(btnH.innerText === "H"){
+        btnH.innerText = "Reading";
+    }
+});
+const btnI = document.getElementById("btnI");
+
+btnI.addEventListener("click", ()=>{
+
+    if(btnI.innerText === "I"){
+        btnI.innerText = "Or";
+    }
+});
+const btnJ = document.getElementById("btnJ");
+
+btnJ.addEventListener("click", ()=>{
+
+    if(btnJ.innerText === "J"){
+        btnJ.innerText = "Are";
+    }
+});
+const btnK = document.getElementById("btnK");
+
+btnK.addEventListener("click", ()=>{
+
+    if(btnK.innerText === "K"){
+        btnK.innerText = "You";
+    }
+});
+const btnL = document.getElementById("btnL");
+
+btnL.addEventListener("click", ()=>{
+
+    if(btnL.innerText === "L"){
+        btnL.innerText = "Just";
+    }
+});
+const btnM = document.getElementById("btnM");
+
+btnM.addEventListener("click", ()=>{
+
+    if(btnM.innerText === "M"){
+        btnM.innerText = "Going";
+    }
+});
+const btnN = document.getElementById("btnN");
+
+btnN.addEventListener("click", ()=>{
+
+    if(btnN.innerText === "N"){
+        btnN.innerText = "Through";
+    }
+});
+const btnO = document.getElementById("btnO");
+
+btnO.addEventListener("click", ()=>{
+
+    if(btnO.innerText === "O"){
+        btnO.innerText = "The";
+    }
+});
+const btnP = document.getElementById("btnP");
+
+btnP.addEventListener("click", ()=>{
+
+    if(btnP.innerText === "P"){
+        btnP.innerText = "Motion";
+    }
+});
+const btnQ = document.getElementById("btnQ");
+
+btnQ.addEventListener("click", ()=>{
+
+    if(btnQ.innerText === "Q"){
+        btnQ.innerText = "of";
+    }
+});
+const btnR = document.getElementById("btnR");
+
+btnR.addEventListener("click", ()=>{
+
+    if(btnR.innerText === "R"){
+        btnR.innerText = "Clicking";
+    }
+});
+const btnS = document.getElementById("btnS");
+
+btnS.addEventListener("click", ()=>{
+
+    if(btnS.innerText === "S"){
+        btnS.innerText = "Through";
+    }
+});
+const btnT = document.getElementById("btnT");
+
+btnT.addEventListener("click", ()=>{
+
+    if(btnT.innerText === "T"){
+        btnT.innerText = "Til";
+    }
+});
+const btnU = document.getElementById("btnU");
+
+btnU.addEventListener("click", ()=>{
+
+    if(btnU.innerText === "U"){
+        btnU.innerText = "The";
+    }
+});
+const btnV = document.getElementById("btnV");
+
+btnV.addEventListener("click", ()=>{
+
+    if(btnV.innerText === "V"){
+        btnV.innerText = "Very";
+    }
+});
+const btnW = document.getElementById("btnW");
+
+btnW.addEventListener("click", ()=>{
+
+    if(btnW.innerText === "W"){
+        btnW.innerText = "Last";
+    }
+});
+const btnX = document.getElementById("btnX");
+
+btnX.addEventListener("click", ()=>{
+
+    if(btnX.innerText === "X"){
+        btnX.innerText = "Button";
+    }
+});
+const btnY = document.getElementById("btnY");
+
+btnY.addEventListener("click", ()=>{
+
+    if(btnY.innerText === "Y"){
+        btnY.innerText = "?";
+    }
+});
+const btnZ = document.getElementById("btnZ");
+
+btnZ.addEventListener("click", ()=>{
+
+    if(btnZ.innerText === "Z"){
+        btnZ.innerText = "Well Are You?";
+    }
+});
+
